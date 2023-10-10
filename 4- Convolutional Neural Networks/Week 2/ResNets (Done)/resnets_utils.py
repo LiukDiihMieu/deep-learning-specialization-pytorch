295b7f0931b0eb22661bcfd0eb0591b7fd07a494
陸地貓 <54393775+LiukDiihMieu@users.noreply.github.com>
2023-10-10 19:47:16 +0900

    Simplify the code a lot.
    Also, make it more compatible with the original tensorflow version.

--- 4- Convolutional Neural Networks/Week 2/ResNets (Done)/resnets_utils.py
+++ 4- Convolutional Neural Networks/Week 2/ResNets (Done)/resnets_utils.py
@@ -1,25 +1,17 @@
-import numpy as np
+# Now it is the same as cnn_utils, which serves to load and format the data.
+
 import h5py
+import torch
+from torch.utils.data import DataLoader, TensorDataset
 
-def load_dataset():
-    train_dataset = h5py.File('datasets/train_signs.h5', "r")
-    train_set_x_orig = np.array(train_dataset["train_set_x"][:]) # your train set features
-    train_set_y_orig = np.array(train_dataset["train_set_y"][:]) # your train set labels
-
-    test_dataset = h5py.File('datasets/test_signs.h5', "r")
-    test_set_x_orig = np.array(test_dataset["test_set_x"][:]) # your test set features
-    test_set_y_orig = np.array(test_dataset["test_set_y"][:]) # your test set labels
-
-    classes = np.array(test_dataset["list_classes"][:]) # the list of classes
+def get_data():
+    train_dataset = h5py.File('datasets/train_signs.h5', 'r')
+    X_train = torch.tensor(train_dataset['train_set_x'][:], dtype=torch.float) / 255
+    X_train = X_train.permute(0, 3, 1, 2)
+    y_train = torch.tensor(train_dataset["train_set_y"][:])
     
-    train_set_y_orig = train_set_y_orig.reshape((1, train_set_y_orig.shape[0]))
-    test_set_y_orig = test_set_y_orig.reshape((1, test_set_y_orig.shape[0]))
-    
-    return train_set_x_orig, train_set_y_orig, test_set_x_orig, test_set_y_orig, classes
-
-
-def convert_to_one_hot(Y, C):
-    Y = np.eye(C)[Y.reshape(-1)].T
-    return Y
-
-
+    test_dataset = h5py.File('datasets/test_signs.h5', 'r')
+    X_test = torch.tensor(test_dataset['test_set_x'][:], dtype=torch.float) / 255
+    X_test = X_test.permute(0, 3, 1, 2)                                                                               
+    y_test = torch.tensor(test_dataset["test_set_y"][:])
+    return X_train, X_test, y_train, y_test
